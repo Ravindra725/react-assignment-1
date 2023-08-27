6aa8f1d20632e4d10bb15472aa7792c32db5b812
Ravindra <krravindra160@gmail.com>
2023-08-27 08:07:21 +0530

    handson-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Hello from './Hello/Hello';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 style={{textShadow: '0px 2px blue'}}>Style using Function and Class Component</h1>
+      <Hello />
     </div>
   );
 }
